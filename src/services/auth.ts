acfa444f3d193622f11b258595920c89a4ef5653
Lioncour <jaekroll@outlook.com>
2025-02-17 23:28:01 +0100

    Update all files

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,3 +1,4 @@
-const REDIRECT_URI = import.meta.env.MODE === 'production'
+const isProd = import.meta.env.MODE === 'production';
+const REDIRECT_URI = isProd
     ? 'https://www.10000beverages.com/oauth2callback'
     : 'http://localhost:5173/oauth2callback'; 
