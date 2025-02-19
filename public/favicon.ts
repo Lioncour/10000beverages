d5d5cd1b6e77db775a23744b5bd03ca96799ff34
Lioncour <jaekroll@outlook.com>
2025-02-19 15:19:57 +0100

    Add counter square, longer hover effect, larger modal images and dynamic favicon

--- public/favicon.ts
+++ public/favicon.ts
@@ -1,1 +1,7 @@
- 
+export async function updateFavicon(imageUrl: string) {
+  const link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+  link.type = 'image/x-icon';
+  link.rel = 'shortcut icon';
+  link.href = imageUrl;
+  document.getElementsByTagName('head')[0].appendChild(link);
+} 
